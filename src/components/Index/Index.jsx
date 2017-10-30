a366854ffe3ed15242f7df6de634c9d4b9eae145
JSLinn727 <justinslinn727@gmail.com>
2017-10-30 17:28:45 -0400

    Linked Index.css to Index.js

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Index.css';
 
 class Index extends Component {
-  render () {
-    return <div className='Index'>
-      ...
-    </div>
+  render() {
+    return <div className="Index">...</div>;
   }
 }
 
-export default Index
+export default Index;
