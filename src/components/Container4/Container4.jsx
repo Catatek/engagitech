71ff4afa3c707b784ca101ba4da2a22f7f211aaf
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-10-31 17:42:36 -0400

    added fonts, made Container4, styled, etc.

---
+++ src/components/Container4/Container4.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import styles from './Container4.css';
+import Hashtag from '../../Assets/hashtag.svg';
+
+class Container4 extends Component {
+  render() {
+    return (
+      <div className="Container4">
+        <div className="div1">
+          <h2>#HASHTAG PRINTING</h2>
+          <p>
+            Hashtag Printing takes guests' photos from digital to physical.
+             Guests snap photos on their phone, post them to Instagram using
+             your #hashtag and their picture prints out at the printing station!
+          </p>
+          <p>
+            Prints are framed and branded with your custom content, providing
+             guests with a memorable keepsake with your brand printed in their
+            hands.
+          </p>
+        </div>
+        <div className="div2">
+          <img src={Hashtag} alt="Hashtag" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Container4;
--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Container2 from '../Container2/Container2';
 import Container3 from '../Container3/Container3';
+import Container4 from '../Container4/Container4';
 
 class Index extends Component {
   render() {
@@ -10,6 +11,7 @@ class Index extends Component {
         <Header />
         <Container2 />
         <Container3 />
+        <Container4 />
       </div>
     );
   }
