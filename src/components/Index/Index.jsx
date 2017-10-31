2462325ef6ec43570a2dd216b7ff8358bf2d1de4
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-10-31 15:26:48 -0400

    bunch of shit.

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
+import VR from '../VR/VR'
 
 class Index extends Component {
   render () {
     return <div className='Index'>
       <Header />
+      <VR />
     </div>
   }
 }
