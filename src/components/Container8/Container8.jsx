59901d85f84d173ecc21788d01971d3355697fb3
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-10-31 21:10:26 -0400

    Added all Container components, files, etc.

---
+++ src/components/Container8/Container8.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './Container8.css';
+
+class Container8 extends Component {
+  render() {
+    return (
+      <div className="Container8">
+        <div className="container8Text" />
+        <div className="selfieImage" />
+      </div>
+    );
+  }
+}
+
+export default Container8;
