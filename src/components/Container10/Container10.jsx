c3c51d6d99b5baed3470227ddd441ec294de538d
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-01 15:38:20 -0400

    Finished container10, styling, etc.

--- src/components/Container10/Container10.jsx
+++ src/components/Container10/Container10.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import styles from './Container10.css';
+import WristBand from '../../Assets/wristBand.png';
 
 class Container10 extends Component {
   render() {
-    return <div className="Container10">...</div>;
+    return (
+      <div className="Container10">
+        <h2 className="container10Header">not all wristwear is created equal.</h2>
+        <div className="container10Row">
+          <div className="wristBand">
+            <img src={WristBand} alt="Engagitech Wristband" />
+          </div>
+          <div className="container10Text">
+            <h2>RFID</h2>
+            <p>Streamline the experience with one wristband.</p>
+            <p>We’ll give your audience a custom wristband at the start  of your event and enable all their curated photo and video  animation to instantly be shared to all of their social channels  with just a swipe of the wrist.</p>
+            <p>Expect an increase of your brand’s reach with our seamless RFID technology.</p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
