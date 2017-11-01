12ad3154d7ad56bfaf1164bfaee69ea02786181b
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-01 15:45:27 -0400

    ...

--- src/components/Container12/Container12.jsx
+++ src/components/Container12/Container12.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import styles from './Container12.css'
 
 class Container12 extends Component {
   render() {
-    return <div className="Container12">...</div>;
+    return (
+    <div className="Container12">
+      <h2>Let's Engage</h2>
+    </div>
+    )
   }
 }
 
--- src/components/Container13/Container13.jsx
+++ src/components/Container13/Container13.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Container13 extends Component {
   render() {
-    return <div className="Container13">...</div>;
+    return (
+    <div className="Container13">
+
+    </div>
+    )
   }
 }
 
