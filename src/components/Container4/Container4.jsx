40fba4fec212962ceffc81f0b9eaddb3e8583746
JSLinn727 <justinslinn727@gmail.com>
2017-11-02 18:43:30 -0400

    hashtag panel media quries

--- src/components/Container4/Container4.jsx
+++ src/components/Container4/Container4.jsx
@@ -2,25 +2,22 @@ import React, { Component } from 'react';
 import styles from './Container4.css';
 import Hashtag from '../../Assets/hashtag.png';
 
-
 class Container4 extends Component {
   render() {
     return (
       <div className="Container4">
-        <div id="container4Id" className="div1">
+        <div className="div1" id="container4Id">
           <h2>#HASHTAG PRINTING</h2>
           <p>
-            Hashtag Printing takes guests' photos from digital to physical.
-             Guests snap photos on their phone, post them to Instagram using
-             your #hashtag and their picture prints out at the printing station!
+            Hashtag Printing takes guests' photos from digital to physical.  Guests snap photos on their phone, post
+            them to Instagram using  your #hashtag and their picture prints out at the printing station!
           </p>
           <p>
-            Prints are framed and branded with your custom content, providing
-             guests with a memorable keepsake with your brand printed in their
-            hands.
+            Prints are framed and branded with your custom content, providing  guests with a memorable keepsake with
+            your brand printed in their hands.
           </p>
         </div>
-        <div id="hashtagPartyColor"className="div2">
+        <div className="div2" id="hashtagPartyColor">
           <img className="hashtagSVG" src={Hashtag} alt="Hashtag" />
         </div>
       </div>
