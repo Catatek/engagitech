37b4d3e05c84d9abf36daceb9a05b7cf5695b1d6
JSLinn727 <justinslinn727@gmail.com>
2017-11-02 19:06:27 -0400

    Fixed hashtag from popping out as well as media station

--- src/components/Container5/Container5.jsx
+++ src/components/Container5/Container5.jsx
@@ -6,7 +6,7 @@ import Phone from '../../Assets/phone.png';
 class Container5 extends Component {
   render() {
     return (
-      <div  id="trigger" className="Container5">
+      <div className="Container5" id="trigger">
         <div className="photoStation">
           <img className="station" src={Station} alt="Photo Booth Station" />
           {/* <img id="object1" src={Phone} alt="phone" /> */}
@@ -15,15 +15,12 @@ class Container5 extends Component {
           <h2>MEDIA STATION</h2>
           <p>Looking for a monster content creator?  </p>
           <p>
-            Look no further; our Media Station is loaded with features that will
-            make  your brand soar. Printed photos, moving GIFs, interactive
-            videos and  everything in between, if you can dream it, we can brand
-            it.
+            Look no further; our Media Station is loaded with features that will make  your brand soar. Printed photos,
+            moving GIFs, interactive videos and  everything in between, if you can dream it, we can brand it.
           </p>
           <p>
-            Your guests will have delicious content flowing out of their pockets
-            and inboxes;  constant reminders of the amazing time they had
-            interacting with your brand.
+            Your guests will have delicious content flowing out of their pockets and inboxes;  constant reminders of the
+            amazing time they had interacting with your brand.
           </p>
         </div>
       </div>
