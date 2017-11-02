c01f5bc09f79e6469a72a4949431175c1b1129db
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-02 11:09:25 -0400

    Components and stuff.

--- src/components/Container13/Container13.jsx
+++ src/components/Container13/Container13.jsx
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import styles from './Container13.css'
+import Logo from '../../Assets/engagitechLogo.png'
 
 class Container13 extends Component {
-  render() {
+  render () {
     return (
-    <div className="Container13">
-
+    <div className='Container13'>
+      <div className="bookNow">
+        <h2>Book Now</h2>
+      </div>
+      <div className="footerLogo">
+        <img src={Logo} alt="Engagitech Logo"/>
+      </div>
+      <div className="container13Row">
+        <div className="socialMedia">
+          <h3>Connect with us</h3>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </div>
+      </div>
     </div>
     )
   }
 }
 
-export default Container13;
+export default Container13
