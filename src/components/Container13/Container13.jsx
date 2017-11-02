7aa229b94d4aabdef0748c2068cf21b98c80cdee
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-02 12:33:35 -0400

    href

--- src/components/Container13/Container13.jsx
+++ src/components/Container13/Container13.jsx
@@ -12,9 +12,9 @@ class Container13 extends Component {
       <div className="container13Row">
         <div className="socialMedia">
           <h3>Connect with us</h3>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
