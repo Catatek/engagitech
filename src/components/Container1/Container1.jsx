64f802d69ec062b14af7ef0968acbd15a0e9f272
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-04 00:47:43 -0400

    ...

---
+++ src/components/Container1/Container1.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styles from './Container1.css'
+
+class Container1 extends Component {
+  render () {
+    return (
+    <div className='Container1'>
+      <p><span className="introHeader">Engagitech</span> is an event marketing firm committed to partnering with companies  who see event marketing as a powerful platform for expanding their brand equity.</p>
+      <p>We use our expertise to strategically enhance engagement, and deepen brand relationships through  high-caliber, interactive, and immersive technology.</p>
+    </div>
+    )
+  }
+}
+
+export default Container1
---
+++ src/components/Container14/Container14.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import styles from './Container14.css'
+
+class Container14 extends Component {
+  render () {
+    return (
+    <div className='Container14'>
+      <div className="container14Text">
+        <h2>Set Creation</h2>
+        <h3>Making your vision to reality.</h3>
+        <p>Our Set Construction team specializes in creating branded environments  that create an immersive experience for your targeted audience.</p>
+        <p>   Our unique metal shapes, custom metal structures, and expert craftsmen  are ready to partner with you.</p>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Container14
