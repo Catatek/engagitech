61880595b8f344c64815087a12fb7ab7ecc94e0a
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-11-06 01:20:23 -0500

    media q.

--- src/components/ScrollingHeader/ScrollingHeader.jsx
+++ src/components/ScrollingHeader/ScrollingHeader.jsx
@@ -6,7 +6,7 @@ class ScrollingHeader extends Component {
   render () {
     return (
     <div className='ScrollingHeader'>
-      <img src={Logo} alt="Engagitech Logo" />
+      <img className="scrollingHeaderLogo" src={Logo} alt="Engagitech Logo" />
       <div className="contactInfo">
         <h4>833.736.4243</h4>
         <a href="https://form.jotform.com/73046790284158" target="_blank"><h3>Contact Us</h3></a>
