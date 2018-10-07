b54ee46d34a8f0439e97a02b96c274f16649a7c0
William Whatley <william@catatek.com>
2018-10-06 21:33:35 -0400

    Added intercom.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
-import 'normalize.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Router from "./Router";
+import registerServiceWorker from "./registerServiceWorker";
+import "normalize.css";
+
+window.Intercom("boot", {
+  app_id: "vtb8kkxp"
+});
+
+window.Intercom("update");
 
 const render = () => {
-  ReactDOM.render(<Router />, document.getElementById('root'));
+  ReactDOM.render(<Router />, document.getElementById("root"));
 };
 
 render();
 
 if (module.hot) {
-  module.hot.accept('./Router', render);
+  module.hot.accept("./Router", render);
 }
